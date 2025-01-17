44cf62e371b966dc3bd8fb3926afa917028ef10f
hvz1961 <vzkhametov@gmail.com>
2025-01-17 10:41:09 +0300

    add new project news

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
 def index(request):
     data = {
@@ -8,7 +6,7 @@ def index(request):
     }
     return render(request, 'main/index.html', data)
 def new(request):
-    return render(request, 'main/new.html')
+    return render(request, 'main/news.html')
 def services(request):
     return render(request, 'main/services.html')
 def contact(request):
